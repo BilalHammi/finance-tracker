f2d08a9a8e9aec8efb4db9dd0f257714b2faf340
Bilal Hammi <bilal.hammi@hva.nl>
2025-10-23 17:26:38 +0200

    Added interface to button component

--- finance-tracker/app/ui/button.component.tsx
+++ finance-tracker/app/ui/button.component.tsx
@@ -1,4 +1,6 @@
-export default function Button({ name, heightCS, widthCS }: { name: string, heightCS: string , widthCS: string  }) {
+import { ButtonProps } from "../../interface/interfaces";
+
+export default function Button({ name, heightCS, widthCS }: ButtonProps) {
   return (
     <button className="bg-black text-white font-bold rounded-lg text-center" style={{ height: heightCS, width: widthCS }}>
       {name}
