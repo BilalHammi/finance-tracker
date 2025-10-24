9b2d433b61a11934dda2d0d938d67db8974c8791
Bilal Hammi <bilal.hammi@hva.nl>
2025-10-24 12:37:01 +0200

    Changed styling + added media  query tablet, mobile and desktop.

--- finance-tracker/app/login/page.tsx
+++ finance-tracker/app/login/page.tsx
@@ -5,18 +5,18 @@ import LoginForm from "../ui/login.component";
 
 export default function Login() {
   return (
-    <div className="flex items-center justify-center mobiel-container h-full">
-       <div className="insidde-mobiel-container">
+    <div className="flex items-center justify-center w-full mobiel-container tablet-container desktop-container h-full ">
+       <div className="insidde-mobiel-container inside-tablet-container bg-white flex justify-start rounded-xl flex-col items-center">
             <Image 
             src={logoImg}
             alt='Logo'
-            className='max-[500px]:w-[50px] max-[500px]:h-[50px] max-[500px]:mt-3'
+            className='max-[600px]:w-[50px] max-[600px]:h-[50px] max-[600px]:mt-3 min-[601px]:h-[60px] min-[601px]:w-[60px]'
             />
-            <h1 className="font-bold text-black max-[500px]:my-5">Welcome Back</h1>
+            <h1 className="font-bold text-[20px] text-black my-5">Welcome Back</h1>
             <div>
               <LoginForm />
             </div>
-            <div className="flex flex-row max-[500px]:text-[12px]">
+            <div className="flex flex-row max-[600px]:text-[12px]">
               <p className="text-gray-400 mr-2">You dont have an Account?</p><Link href="/register" className="text-black">Register Here!</Link>
             </div>
         </div>
--- finance-tracker/app/ui/login.component.tsx
+++ finance-tracker/app/ui/login.component.tsx
@@ -8,18 +8,18 @@ import Image from "next/image";
 
 export default function loginForm() {
     return (
-       <div className="max-[500px]:flex max-[500px]:flex-col">
+       <div className="max-[600px]:flex max-[600px]:flex-col">
         <div>
-            <div className="max-[500px]:flex max-[500px]:flex-row">
-                <Image src={EmailIcon} alt="Email icon" className="w-6 h-6"/>
-                <p className="max-[500px]:ml-1 max-[500px]:mb-2 max-[500px]:text-gray-400">Email</p>
+            <div className="flex flex-row mb-2">
+                <Image src={EmailIcon} alt="Email icon" className="h-7 w-7"/>
+                <p className="ml-1 text-gray-400">Email</p>
             </div>
             <InputComponent widthCS="243px" heightCS="40px" typeInput="text" name="John.Doe@example.com"/>
         </div>
         <div>
-            <div className="max-[500px]:flex max-[500px]:flex-row">
-                <Image src={PasswordIcon} alt="Lock icon" className="w-6 h-6"/>
-                <p className="max-[500px]:ml-1 max-[500px]:mb-2 max-[500px]:text-gray-400">Password</p>
+            <div className="flex flex-row mb-2">
+                <Image src={PasswordIcon} alt="Lock icon" className="w-7 h-7"/>
+                <p className=" text-gray-400 ml-1">Password</p>
             </div>
             <div className="flex flex-row">
                 <Image  src={PasswordDot} alt="Password icon" className="w-3 h-3 absolute ml-52 mt-3"/>
